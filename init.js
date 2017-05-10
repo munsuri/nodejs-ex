3695b24e07ac7d88b7608e318e94288e118b16aa
Iván Munsuri Ibañez <munsuri@gmail.com>
2017-05-10 12:16:51 -0500

    Trying auto env config.

---
+++ init.js
@@ -0,0 +1,33 @@
+if( !process.env.ROOT_URL )
+  process.env['ROOT_URL'] = 'http://nodejs-mongo-persistent-spoteet.1d35.starter-us-east-1.openshiftapps.com';
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+if( !process.env.PORT )
+  process.env['PORT'] = port;
+
+var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+var mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
+
+if( mongoURL == null && process.env.DATABASE_SERVICE_NAME ){
+  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+      mongoUser = process.env[mongoServiceName + '_USER'];
+
+  if (mongoHost && mongoPort && mongoDatabase) {
+    mongoURLLabel = mongoURL = 'mongodb://';
+    if (mongoUser && mongoPassword) {
+      mongoURL += mongoUser + ':' + mongoPassword + '@';
+    }
+    // Provide UI label that excludes user id and pw
+    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+  }
+  process.env['MONGO_URL'] = mongoURL;
+}
+
+require('main.js');
